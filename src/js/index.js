095a7d2e2cba2f285604a42bdbf8afd3e2ec2cb9
Andrey Brekotkin <brekot@ya.ru>
2020-04-30 12:34:43 +0300

    Первый

---
+++ src/js/index.js
@@ -0,0 +1,25 @@
+//import $ from "jquery";
+//import Swiper from 'swiper';
+
+//import { Swiper, Navigation, Pagination } from 'swiper/dist/js/swiper.esm.js';
+//Swiper.use([Navigation, Pagination]);
+
+//window.jQuery = $;
+//window.$ = $;
+//require('@fancyapps/fancybox');
+
+//$(function() {
+
+    /* - - - Подключение fancybox - - - */
+/*     $('[data-fancybox]').fancybox({
+        buttons: [
+            "zoom",
+            //"share",
+            "slideShow",
+            "fullScreen",
+            //"download",
+            //"thumbs",
+            "close"
+        ],
+    }); */
+//});
