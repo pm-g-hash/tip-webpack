303c2500b7adb81f333ef916eb9291161a1cd776
Andrey Brekotkin <brekot@ya.ru>
2020-05-11 01:00:02 +0300

    Index - full

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
 import $ from "jquery";
 import Swiper from './swiper.js';
 
+window.jQuery = $;
+window.$ = $;
+
 $(function() {
 
-	/* Прикрепляем шапку */
+	// Прикрепляем шапку
     var headerOffsetTop = $(".main-header").offset().top;
 
     $(window).scroll(function () {
@@ -13,6 +16,27 @@ $(function() {
         if (winTop > headerOffsetTop) $(".main-header").addClass("main-header_fixed");
 		else $(".main-header").removeClass("main-header_fixed");
     });
+
+    // Меню
+    if ($(window).width() < 768)
+    {
+        $('.menu-main-btn').click(function(){
+
+           // $('.menu-main').css('max-height', $(window).height() - 114);
+
+            $('.menu-main').slideToggle();
+        });   
+    }
+
+    if ($(window).width() < 992)
+    {
+        $('.menu-main__lvl1-link_parent').click(function(e){
+
+            e.preventDefault();
+
+            $(this).next('.menu-main__lvl2').slideToggle();
+        });
+    }
 
     // Главный слайдер
     let intSlideMainCount = $('.slider-main__slide').length;
@@ -43,8 +67,6 @@ $(function() {
         let intPrev = intActive - 1;
         let intNext = intActive + 1;
 
-        console.log(intActive + ';' + intPrev);
-
         if (intPrev < 1) intPrev = intSlideMainCount + intPrev;
         if (intNext > intSlideMainCount) intNext = intNext - intSlideMainCount;
 
@@ -52,12 +74,40 @@ $(function() {
         $('.slider-main__count-next').text(intNext + ' / ' + intSlideMainCount);
     });
 
+    // Слайдер как мы работаем
+    if ($(window).width() < 992)
+    {
+        new Swiper('.we-working__slider', {
+            loop: true,
+            slidesPerView: 1,
+            spaceBetween: 20,
+            breakpoints: {
+                576: {
+                    slidesPerView: 2,
+                    spaceBetween: 24,
+                },
+            },
+            pagination: {
+                clickable: true,
+                el: '.we-working__pagination',
+                bulletClass: 'slider-pagination__point',
+                bulletActiveClass: 'slider-pagination__point_active',
+            },
+        });
+    }
+
     // Слайдер отзывы
     new Swiper('.review-slider__slider', {
         loop: true,
         loopAdditionalSlides: 1,
-        slidesPerView: 2,
-        spaceBetween: 40,
+        slidesPerView: 1,
+        spaceBetween: 75,
+        breakpoints: {
+            992: {
+                slidesPerView: 2,
+                spaceBetween: 40,
+            },
+        },
         navigation: {
             prevEl: '.review-slider__prev',
             nextEl: '.review-slider__next',
@@ -73,8 +123,19 @@ $(function() {
     // Слайдер клиенты
     new Swiper('.client-slider__slider', {
         loop: true,
-        slidesPerView: 5,
+        loopAdditionalSlides: 1,
+        slidesPerView: 1,
         spaceBetween: 10,
+        breakpoints: {
+            576: {
+                slidesPerView: 3,
+                spaceBetween: 10,
+            },
+            992: {
+                slidesPerView: 5,
+                spaceBetween: 10,
+            },
+        },
         pagination: {
             clickable: true,
             el: '.client-slider__pagination',
@@ -84,35 +145,61 @@ $(function() {
     });
 
     // Слайдер новостей
-    let intSliderNewsCount = $('.news-slider__slide').length / 3;
+    let intDelete = 3;
+
+    if ($(window).width() < 576)
+    {
+        intDelete = 1;
+    }
+    else if ($(window).width() < 992)
+    {
+        intDelete = 2;
+    }
+
+    let intSliderNewsCount = Math.ceil($('.news-slider__slide').length / intDelete);
+
+    $('.news-slider__count').text('1 / ' + intSliderNewsCount);
 
     let sliderNews = new Swiper('.news-slider__slider', {
-        slidesPerView: 3,
-        slidesPerGroup: 3,
-        spaceBetween: 25,
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        spaceBetween: 20,
+        breakpoints: {
+            576: {
+                slidesPerView: 2,
+                slidesPerGroup: 2,
+                spaceBetween: 30,
+            },
+            992: {
+                slidesPerView: 3,
+                slidesPerGroup: 3,
+                spaceBetween: 25,
+            },
+        },
         navigation: {
             prevEl: '.news-slider__prev',
             nextEl: '.news-slider__next',
             disabledClass: 'str-disabled'
         },
+        on: {
+            imagesReady: function () {
+
+                let intSliderNewsElemHeight = 0;
+
+                $('.news-slider__item').each(function(){
+
+                    if ($(this).height() > intSliderNewsElemHeight)
+                    {
+                        intSliderNewsElemHeight = $(this).height();
+                    }
+                });
+
+                $('.news-slider__item').height(intSliderNewsElemHeight);
+            },
+            slideChange: function () {
+
+                $('.news-slider__count').text(Math.ceil(sliderNews.activeIndex / intDelete + 1) + ' / ' + intSliderNewsCount);
+            },
+        }
     });
-
-    $('.news-slider__count').text('1 / ' + intSliderNewsCount);
-
-    sliderNews.on('slideChange', function () {
-
-        $('.news-slider__count').text((sliderNews.activeIndex / 3 + 1) + ' / ' + intSliderNewsCount);
-    });
-
-//    let intSliderNewsElemHeight = 0;
-//
-//    $('.news-slider__item').each(function(){
-//
-//        if ($(this).height() > intSliderNewsElemHeight)
-//        {
-//            intSliderNewsElemHeight = $(this).height();
-//        }
-//    });
-//
-//    $('.news-slider__item').height(intSliderNewsElemHeight);
 });
